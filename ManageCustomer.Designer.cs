62545805cdf83b354fb50dc7814a120ccc53b53a
ASM_2nd_Database_L-NG-C-TI-N <Lntien111203@gmail.com>
2024-12-19 23:35:20 +0700

    init project

---
+++ ManageCustomer.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace CUSTOMER_MANAGERMENT
+{
+    partial class ManageCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbCustomerManager = new System.Windows.Forms.GroupBox();
+            this.dtgCustomer = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.txtCustomerAddress = new System.Windows.Forms.TextBox();
+            this.txtPhonenumber = new System.Windows.Forms.TextBox();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtCustomerCode = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblCustomerAddress = new System.Windows.Forms.Label();
+            this.lblPhonenumber = new System.Windows.Forms.Label();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.lblCustomerCode = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.gbCustomerManager.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbCustomerManager
+            // 
+            this.gbCustomerManager.Controls.Add(this.dtgCustomer);
+            this.gbCustomerManager.Controls.Add(this.label1);
+            this.gbCustomerManager.Controls.Add(this.txtSearch);
+            this.gbCustomerManager.Controls.Add(this.txtCustomerAddress);
+            this.gbCustomerManager.Controls.Add(this.txtPhonenumber);
+            this.gbCustomerManager.Controls.Add(this.txtCustomerName);
+            this.gbCustomerManager.Controls.Add(this.txtCustomerCode);
+            this.gbCustomerManager.Controls.Add(this.btnClear);
+            this.gbCustomerManager.Controls.Add(this.btnDelete);
+            this.gbCustomerManager.Controls.Add(this.btnUpdate);
+            this.gbCustomerManager.Controls.Add(this.btnAdd);
+            this.gbCustomerManager.Controls.Add(this.lblCustomerAddress);
+            this.gbCustomerManager.Controls.Add(this.lblPhonenumber);
+            this.gbCustomerManager.Controls.Add(this.lblCustomerName);
+            this.gbCustomerManager.Controls.Add(this.lblCustomerCode);
+            this.gbCustomerManager.Location = new System.Drawing.Point(35, 35);
+            this.gbCustomerManager.Name = "gbCustomerManager";
+            this.gbCustomerManager.Size = new System.Drawing.Size(714, 378);
+            this.gbCustomerManager.TabIndex = 0;
+            this.gbCustomerManager.TabStop = false;
+            this.gbCustomerManager.Text = "Customer Manager";
+            // 
+            // dtgCustomer
+            // 
+            this.dtgCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgCustomer.Location = new System.Drawing.Point(360, 46);
+            this.dtgCustomer.Name = "dtgCustomer";
+            this.dtgCustomer.RowHeadersWidth = 62;
+            this.dtgCustomer.RowTemplate.Height = 28;
+            this.dtgCustomer.Size = new System.Drawing.Size(348, 326);
+            this.dtgCustomer.TabIndex = 14;
+            this.dtgCustomer.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgCustomer_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(497, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Search";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(584, 14);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 26);
+            this.txtSearch.TabIndex = 12;
+            // 
+            // txtCustomerAddress
+            // 
+            this.txtCustomerAddress.Location = new System.Drawing.Point(191, 193);
+            this.txtCustomerAddress.Name = "txtCustomerAddress";
+            this.txtCustomerAddress.Size = new System.Drawing.Size(100, 26);
+            this.txtCustomerAddress.TabIndex = 11;
+            // 
+            // txtPhonenumber
+            // 
+            this.txtPhonenumber.Location = new System.Drawing.Point(191, 144);
+            this.txtPhonenumber.Name = "txtPhonenumber";
+            this.txtPhonenumber.Size = new System.Drawing.Size(100, 26);
+            this.txtPhonenumber.TabIndex = 10;
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(191, 87);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(100, 26);
+            this.txtCustomerName.TabIndex = 9;
+            // 
+            // txtCustomerCode
+            // 
+            this.txtCustomerCode.Location = new System.Drawing.Point(191, 46);
+            this.txtCustomerCode.Name = "txtCustomerCode";
+            this.txtCustomerCode.Size = new System.Drawing.Size(100, 26);
+            this.txtCustomerCode.TabIndex = 8;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(119, 322);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 34);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(6, 322);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 34);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(119, 253);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 36);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(6, 253);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 36);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblCustomerAddress
+            // 
+            this.lblCustomerAddress.AutoSize = true;
+            this.lblCustomerAddress.Location = new System.Drawing.Point(6, 199);
+            this.lblCustomerAddress.Name = "lblCustomerAddress";
+            this.lblCustomerAddress.Size = new System.Drawing.Size(141, 20);
+            this.lblCustomerAddress.TabIndex = 3;
+            this.lblCustomerAddress.Text = "Customer Address";
+            // 
+            // lblPhonenumber
+            // 
+            this.lblPhonenumber.AutoSize = true;
+            this.lblPhonenumber.Location = new System.Drawing.Point(6, 144);
+            this.lblPhonenumber.Name = "lblPhonenumber";
+            this.lblPhonenumber.Size = new System.Drawing.Size(113, 20);
+            this.lblPhonenumber.TabIndex = 2;
+            this.lblPhonenumber.Text = "Phone number";
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(6, 90);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(124, 20);
+            this.lblCustomerName.TabIndex = 1;
+            this.lblCustomerName.Text = "Customer Name";
+            // 
+            // lblCustomerCode
+            // 
+            this.lblCustomerCode.AutoSize = true;
+            this.lblCustomerCode.Location = new System.Drawing.Point(6, 46);
+            this.lblCustomerCode.Name = "lblCustomerCode";
+            this.lblCustomerCode.Size = new System.Drawing.Size(120, 20);
+            this.lblCustomerCode.TabIndex = 0;
+            this.lblCustomerCode.Text = "Customer Code";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(35, 420);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 28);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // ManageCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.gbCustomerManager);
+            this.Name = "ManageCustomer";
+            this.Text = "ManageCustomer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.ManageCustomer_Load);
+            this.gbCustomerManager.ResumeLayout(false);
+            this.gbCustomerManager.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCustomer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbCustomerManager;
+        private System.Windows.Forms.TextBox txtCustomerAddress;
+        private System.Windows.Forms.TextBox txtPhonenumber;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtCustomerCode;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblCustomerAddress;
+        private System.Windows.Forms.Label lblPhonenumber;
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.Label lblCustomerCode;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dtgCustomer;
+    }
+}
---
+++ ManageCustomer.cs
@@ -0,0 +1,243 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CUSTOMER_MANAGERMENT
+{
+    public partial class ManageCustomer : Form
+    {
+        private int customerId;
+        private int employeeId;
+        private string authorityLevel;
+        private int userId;
+        public ManageCustomer(string authorityLevel, int employeeId)
+        {
+            InitializeComponent();
+            this.employeeId = employeeId;
+            this.authorityLevel = authorityLevel;
+        }
+ 
+        private void ClearData()
+        {
+            FlushCustomerId();
+            txtCustomerCode.Text = string.Empty;
+            txtCustomerName.Text = string.Empty;
+            txtPhonenumber.Text = string.Empty;
+            txtCustomerAddress.Text = string.Empty;
+            txtCustomerCode.Focus();
+
+        }
+        private void ChangeButtonStatus(bool buttonStatus)
+        {
+            // When customer is selected, button add will be disabled
+            // button Update, Delete & Clear will be enabled and vice versa
+
+            btnUpdate.Enabled = buttonStatus;
+            btnDelete.Enabled = buttonStatus;
+            btnClear.Enabled = buttonStatus;
+            btnAdd.Enabled = !buttonStatus;
+        }
+        private bool ValidateData(string customerCode, string customerName, string customerAddress, string phoneNumber)
+        {
+            // Validate user input data
+            // Declare isValid variable to check. First we assume all data is valid and check each of it
+            bool isValid = true;
+
+            if (customerCode == null || customerCode == string.Empty)
+            {
+                MessageBox.Show(
+                    "Customer Code cannot be blank",
+                    "Warning",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                isValid = false;
+                txtCustomerCode.Focus();
+            }
+            else if (customerName == null || customerName == string.Empty)
+            {
+                MessageBox.Show(
+                    "Customer Name cannot be blank",
+                    "Warning",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                isValid = false;
+                txtCustomerName.Focus();
+            }
+            else if (customerAddress == null || customerAddress == string.Empty)
+            {
+                MessageBox.Show(
+                    "Customer Address cannot be blank",
+                    "Warning",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                isValid = false;
+                txtCustomerAddress.Focus();
+            }
+            else if (phoneNumber == null || phoneNumber == string.Empty)
+            {
+                MessageBox.Show(
+                    "Phone number cannot be blank",
+                    "Warning",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                isValid = false;
+                txtPhonenumber.Focus();
+            }
+
+            return isValid;
+
+        }
+        private void FlushCustomerId()
+        {
+            // Flush customerID value to check button and setup status for buttons
+            this.customerId = 0;
+            ChangeButtonStatus(false);
+        }
+        private void LoadCustomerData()
+        {
+            // Open connection by call the GetConnection function in DatabaseConnection class
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-D80GRN6\\SQLEXPRESS;Initial Catalog=\"badminton racket sales product manager\";Integrated Security=True;Trust Server Certificate=True");
+
+            // Check connection
+            if (connection != null)
+            {
+                // Open the connection
+                connection.Open();
+
+                // Declare query to the database
+                string query = "SELECT * FROM Customer";
+
+                // Initialize SqlDataAdapter to translate query result to a data table
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+
+                // Initialize data table
+                DataTable table = new DataTable();
+
+                // Fill the data table by data queried from the database
+                adapter.Fill(table);
+
+                // Set the datasource of data gridview by the dataset
+                dtgCustomer.DataSource = table;
+
+                // close the connection
+                connection.Close();
+            }
+        }
+        private bool CheckUserExistence(int customerId)
+        {
+            bool isExist = false;
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-D80GRN6\\SQLEXPRESS;Initial Catalog=\"badminton racket sales product manager\";Integrated Security=True;Trust Server Certificate=True");
+
+            if (connection != null)
+            {
+                connection.Open();
+                string query = "SELECT * FROM Customer WHERE CustomerID = @customerId";
+
+                // Declare SqlCommand variable to add parameters to query and execute it
+                SqlCommand command = new SqlCommand(query, connection);
+
+                // Add parameters
+                command.Parameters.AddWithValue("@customerId", customerId);
+
+                // Declare SqlDataReader variable to read retrieved data
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Check if reader has row (query success and return one row show user information)
+                isExist = reader.HasRows;
+
+                connection.Close();
+            }
+
+            return isExist;
+        }
+        private void AddCustomer(string customerCode, string customerName, string customerAddress, string phoneNumber)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-D80GRN6\\SQLEXPRESS;Initial Catalog=\"badminton racket sales product manager\";Integrated Security=True;Trust Server Certificate=True");
+
+            if (connection != null)
+            {
+                connection.Open();
+                string query = "INSERT INTO Customer (CustomerCode, CustomerName, PhoneNumber, Address) " +
+                               "VALUES (@customerCode, @customerName, @phoneNumber, @customerAddress)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@customerCode", customerCode);
+                command.Parameters.AddWithValue("@customerName", customerName);
+                command.Parameters.AddWithValue("@phoneNumber", phoneNumber);
+                command.Parameters.AddWithValue("@customerAddress", customerAddress);
+                int result = command.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    // Success message
+                    MessageBox.Show(
+                 "Successfully add new customer",
+                 "Success",
+                 MessageBoxButtons.OK,
+                 MessageBoxIcon.Information
+                 );
+                }
+                else
+                {
+                    // Error message
+                    MessageBox.Show(
+                 "An error occur while adding customer",
+                 "Error",
+                 MessageBoxButtons.OK,
+                 MessageBoxIcon.Error
+                 );
+                }
+                connection.Close();
+                ClearData();
+                // Reload the data gridview
+                LoadCustomerData();
+            }
+        }
+
+        private void ManageCustomer_Load(object sender, EventArgs e)
+        {
+            LoadCustomerData();
+        }
+
+        private void dtgCustomer_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = dtgCustomer.CurrentCell.RowIndex;
+            if (index > -1)
+            {
+                customerId = (int)dtgCustomer.Rows[index].Cells[0].Value;
+                txtCustomerCode.Text = dtgCustomer.Rows[index].Cells[1].Value.ToString();
+                txtCustomerName.Text = dtgCustomer.Rows[index].Cells[2].Value.ToString();
+                txtPhonenumber.Text = dtgCustomer.Rows[index].Cells[3].Value.ToString();
+                txtCustomerAddress.Text = dtgCustomer.Rows[index].Cells[4].Value.ToString();
+                ChangeButtonStatus(true);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Get data from user input
+            string customerCode = txtCustomerCode.Text;
+            string customerName = txtCustomerName.Text;
+            string customerAddress = txtCustomerAddress.Text;
+            string phoneNumber = txtPhonenumber.Text;
+
+            // Validate data
+            bool isValid = ValidateData(customerCode, customerName, customerAddress, phoneNumber);
+            if (isValid)
+            {
+                AddCustomer(customerCode, customerName, customerAddress, phoneNumber);
+            }
+        }
+
+       
+    }
+}
